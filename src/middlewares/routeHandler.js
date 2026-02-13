90a5c34ef3273a21d56248f0a36a204cd5aece0a
Jandavi <rodriguesjandavi193@gmail.com>
2026-02-12 22:18:03 -0300

    middleware the routes

---
+++ src/middlewares/routeHandler.js
@@ -0,0 +1,16 @@
+import { routes } from "../routes/index.js";
+
+export function routeHandler(request, response) {
+  const route = routes.find((route) => {
+    // função pra verificar se está tudo nos conformes as rotas e requisições
+    return route.method === request.method && route.path === request.url
+  })
+  // se sim, faz isso:
+  if (route) {
+    return route.controller(request, response)
+  }
+
+    // se não, faz isso:
+   return response.wiriteHead(404).end()
+
+}
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import http from "node:http"
 
 import { jsonHandler } from "./middlewares/jsonHandler.js"
+import { routeHandler } from "./middlewares/routeHandler.js"
 
 async function listener(request, response) {
   await jsonHandler(request, response)
-  console.log(request.body)
+  // passa aqui as requisições e as respostas que conferimos se esava certo la no routeHandler.js
+  routeHandler(request, response)
 }
 
 http.createServer(listener).listen(3333)
