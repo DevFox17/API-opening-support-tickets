17a5fc1271b97eaa22cbb0fa849a45a932d93416
Jandavi <rodriguesjandavi193@gmail.com>
2026-02-14 01:28:22 -0300

    riding object the tickets

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,4 +1,20 @@
+ import { randomUUID } from "node:crypto"
+ 
  // Funçao que executa algo quando a nossa rota for chamada
 export function create( {request, response} ) {
-  return response.end("Criado com sucesso!")
+  const { equipment, description, user_name } = request.body
+
+  // isso aqui o usuário que vai colocar ( equipment, description, etc. Menos o id:, created_at etc )
+  const ticket = {
+    // Padrão de identificador único ( cria id único)
+    id: randomUUID(),
+    equipment,
+    description,
+    user_name,
+    status: "open",
+    created_at: new Date(),
+    updated_at: new Date()
+  }
+
+  return response.end(JSON.stringify(ticket))
 }
