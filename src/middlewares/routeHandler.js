c1768d8cad95d9f54ae87ffc465802910f65d628
Jandavi <rodriguesjandavi193@gmail.com>
2026-02-20 13:24:10 -0300

    parameter dinamics and obting parametrs the outhers routes

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -1,5 +1,6 @@
 import { routes } from "../routes/index.js";
 import { Database } from "../database/database.js";
+import { extractQueryParams } from "../utils/extractQueryParams.js";
 
 // instancia o Database
 const database = new Database()
@@ -7,14 +8,20 @@ const database = new Database()
 export function routeHandler(request, response) {
   const route = routes.find((route) => {
     // função pra verificar se está tudo nos conformes as rotas e requisições
-    return route.method === request.method && route.path === request.url
+    return route.method === request.method && route.path.test(request.url)
   })
   // se sim, faz isso:
   if (route) {
-    return route.controller({request, response, database})
+    const routeParams = request.url.match(route.path)
+
+    const { query } = routeParams.groups
+
+    request.query = query ? extractQueryParams(query) : {}
+
+    return route.controller({request, response, database,})
   }
 
     // se não, faz isso:
-  return response.wiriteHead(404).end()
+  return response.writeHead(404).end()
 
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,9 @@
 // Responsável por juntar todas as rotas que vão estar em aquivos diferentes
 import { tickets } from "./tickest.js";
+import { parseRoutePath } from "../utils/parseRoutePath.js";
 
-export const routes = [...tickets]
+export const routes = [...tickets].map((route) => ({
+  ...route,
+  path: parseRoutePath(route.path),
+}))
 
